542d489e4128cc8fd37c92a885e78a1af7a351af
wanxwang <wanxin-wang@uiowa.edu>
2018-06-25 10:52:18 -0500

    vrlink integration 1

---
+++ Assets/scripts/ClockStaticAln.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class ClockStaticAln
+{
+    private int m_tick0;
+    private int m_tm0;
+    public ClockStaticAln()
+    {
+    }
+    public void StartClock()
+    {
+        DateTime now = DateTime.Now;
+        int[] v = { now.Hour,   now.Minute, now.Second, now.Millisecond };
+        int[] w = { 60,         60,         1000,       1};
+        m_tm0 = 0;
+        for (int i = 0; i < v.Length; i++)
+        {
+            m_tm0 += v[i];
+            m_tm0 *= w[i];
+        }
+
+        m_tick0 = Environment.TickCount;
+    }
+
+    public int GetTickCnt()
+    {
+        int delta = Environment.TickCount - m_tick0;
+        return delta + m_tm0;
+    }
+
+};
+
--- Assets/scripts/VrlinkDisDynamic.cs
+++ Assets/scripts/VrlinkDisDynamic.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-
+using makVrl;
 struct GlobalId
 {
     public uint owner; //owner's ip
@@ -13,9 +13,129 @@ struct GlobalId
         owner = a_owner;
         objId = a_objId;
     }
+    public GlobalId(ushort a_siteId, ushort a_hostId, ushort a_objId)
+    {
+        //little endian layout: xxx.xxx.xxx.xxx
+        //                     |siteId |hostId |
+        owner = (uint)(a_siteId | (a_hostId << 16));
+        objId = a_objId;
+    }
 };
 class VrlinkDisDynamic
 {
+    struct VrlinkConf
+    {
+        public int drAlgor;
+        public int stmType;
+        public bool smoothOn;
+
+        public double translationThreshold;
+        public double rotationThreshold;
+
+        public double latitude;
+        public double longitude;
+        public double viewOriPsi;
+        public double viewOriTheta;
+        public double viewOriPhi;
+
+        //entity type
+        public int kind;
+        public int domain;
+        public int country;
+        public int category;
+
+        public int subCategory;
+        public int specific;
+        public int extra;
+    };
+
+    struct EntityPub
+    {
+        public EntityPublisher pub;
+        public EntityStateRepository esr;
+    };
+
+    struct EntityState
+    {
+        public Vector3 pos;
+        public Vector3 forward;
+        public Vector3 right;
+        public bool updated;
+    };
+
+    struct CnnOut
+    {
+        public ExerciseConnection cnn;
+        public Dictionary<ushort, EntityPub> pubs;
+    };
+
+    struct EntityPubRT
+    {
+        public ExerciseConnection cnn;
+        public EntityPublisher pub;
+        public EntityStateRepository esr;
+        public EntityPubRT(ExerciseConnection a_cnn, EntityPublisher a_pub, EntityStateRepository a_esr)
+        {
+            cnn = a_cnn;
+            pub = a_pub;
+            esr = a_esr;
+        }
+    };
+
+    Dictionary<uint, CnnOut> m_cnnsOut;
+    Dictionary<GlobalId, EntityState> m_statesIn;
+
+    ExerciseConnection m_cnnIn;
+    ReflectedEntityList m_entitiesIn;
+
+    static VrlinkConf s_disConf;
+    ClockStaticAln m_sysClk;
+    uint m_self;
+
+    bool getEntityPub(uint ip, GlobalId id_global, out EntityPubRT pubRT)
+    {
+        ushort objId = id_global.objId;
+        CnnOut cnnout;
+        bool cnnHit = false;
+        bool pubHit = false;
+        EntityPub epb;
+        if ((cnnHit = m_cnnsOut.TryGetValue(ip, out cnnout))
+            && (pubHit = cnnout.pubs.TryGetValue(objId, out epb)))
+        {
+            ExerciseConnection cnn = cnnout.cnn;
+            EntityStateRepository esr = epb.esr;
+            EntityPublisher pub = epb.pub;
+            pubRT = new EntityPubRT(cnn, pub, esr);
+            return true;
+        }
+        else
+        {
+            pubRT = new EntityPubRT(null, null, null);
+            return false;
+        }
+    }
+
+    GlobalId VrlinkId2GlobalId(string entityId)
+    {
+        char [] seperator = {':'};
+        string[] segs = entityId.Split(seperator);
+        ushort siteId = 0;
+        ushort hostId = 0;
+        ushort entityNum = 0;
+        if (UInt16.TryParse(segs[0], out siteId)
+            && UInt16.TryParse(segs[1], out hostId)
+            && UInt16.TryParse(segs[2], out entityNum))
+        {
+            GlobalId id_global = new GlobalId(siteId, hostId, entityNum);
+            return id_global;
+        }
+        else
+        {
+            Debug.Assert(false); //entityId is not in right format
+            return new GlobalId(0, 0, 0);
+        }
+    }
+
     public bool Receive(GlobalId id_global, out Vector3 pos, out Vector3 tan, out Vector3 right)
     {
         //todo: recieve state information through vrlink
@@ -46,7 +166,7 @@ class VrlinkDisDynamic
 
         byte[] seg = BitConverter.GetBytes(self);
         Debug.LogFormat("self:\t{0}.{1}.{2}.{3}", seg[0], seg[1], seg[2], seg[3]);
-                               
+
     }
 
     public void NetworkUnInitialize()
