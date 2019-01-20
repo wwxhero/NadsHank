dde196e0ef1843fe74089820e321830d34c94108
wanxwang <wanxin-wang@uiowa.edu>
2019-01-19 21:52:29 -0600

    mock tracker intialized

--- Assets/Plugins/RootMotion/FinalIK/_DEMOS/VRIK/Scripts/VRIKCalibrationController.cs
+++ Assets/Plugins/RootMotion/FinalIK/_DEMOS/VRIK/Scripts/VRIKCalibrationController.cs
@@ -8,21 +8,29 @@ namespace RootMotion.Demos
     public class VRIKCalibrationController : MonoBehaviour
     {
 
-        [Tooltip("Reference to the VRIK component on the avatar.")] public VRIK ik;
-        [Tooltip("The settings for VRIK calibration.")] public VRIKCalibrator.Settings settings;
-        [Tooltip("The HMD.")] public Transform headTracker;
-        [Tooltip("(Optional) A tracker placed anywhere on the body of the player, preferrably close to the pelvis, on the belt area.")] public Transform bodyTracker;
-        [Tooltip("(Optional) A tracker or hand controller device placed anywhere on or in the player's left hand.")] public Transform leftHandTracker;
-        [Tooltip("(Optional) A tracker or hand controller device placed anywhere on or in the player's right hand.")] public Transform rightHandTracker;
-        [Tooltip("(Optional) A tracker placed anywhere on the ankle or toes of the player's left leg.")] public Transform leftFootTracker;
-        [Tooltip("(Optional) A tracker placed anywhere on the ankle or toes of the player's right leg.")] public Transform rightFootTracker;
+        [Tooltip("Reference to the VRIK component on the avatar.")]
+        public VRIK ik;
+        [Tooltip("The settings for VRIK calibration.")]
+        public VRIKCalibrator.Settings settings;
+        [Tooltip("The HMD.")]
+        public Transform headTracker;
+        [Tooltip("(Optional) A tracker placed anywhere on the body of the player, preferrably close to the pelvis, on the belt area.")]
+        public Transform bodyTracker;
+        [Tooltip("(Optional) A tracker or hand controller device placed anywhere on or in the player's left hand.")]
+        public Transform leftHandTracker;
+        [Tooltip("(Optional) A tracker or hand controller device placed anywhere on or in the player's right hand.")]
+        public Transform rightHandTracker;
+        [Tooltip("(Optional) A tracker placed anywhere on the ankle or toes of the player's left leg.")]
+        public Transform leftFootTracker;
+        [Tooltip("(Optional) A tracker placed anywhere on the ankle or toes of the player's right leg.")]
+        public Transform rightFootTracker;
 
         [Header("Data stored by Calibration")]
         public VRIKCalibrator.CalibrationData data = new VRIKCalibrator.CalibrationData();
 
         void LateUpdate()
         {
-            if (Input.GetKeyDown(KeyCode.C))
+            if (Input.GetKeyDown(KeyCode.A))
             {
                 // Calibrate the character, store data of the calibration
                 data = VRIKCalibrator.Calibrate(ik, settings, headTracker, bodyTracker, leftHandTracker, rightHandTracker, leftFootTracker, rightFootTracker);
