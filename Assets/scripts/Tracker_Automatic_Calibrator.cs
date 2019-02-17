4a317bf3d0ab4f1c1fe43679aaf68953ef688995
Soumyajit Chakraborty <soumyajit-chakraborty@uiowa.edu>
2019-02-17 09:47:52 -0600

    Tracker rotation during calibration is fixed

---
+++ Assets/scripts/Tracker_Automatic_Calibrator.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+using RootMotion.Demos;
+
+public class Tracker_Automatic_Calibrator : MonoBehaviour {
+    private GameObject leftHand, rightHand, leftLeg, rightLeg, pelvis, head;
+    private Quaternion leftHand_Default_Rotation, left_Foot_Default_Rotation, rightHand_Default_Rotation, right_Foot_Default_Rotation, pelvis_Default_Rotation, head_Default_Rotation;
+    private Quaternion leftHand_Updated_Rotation, left_Foot_Updated_Rotation, rightHand_Updated_Rotation, right_Foot_Updated_Rotation, pelvis_Updated_Rotation, head_Updated_Rotation;
+    public model_and_Steam_VR_Controller m1;
+    public GameObject SteamVR_Activator, left_Hand_Target, right_Hand_Target, left_Foot_Target, right_Foot_Target, pelvis_Target, head_Target;
+    private bool configuration;
+
+    // Use this for initialization
+    void Start () {
+        leftHand = GameObject.Find(this.gameObject.name + "/CMU compliant skeleton/Hips/LowerBack/Spine/Spine1/LeftShoulder/LeftArm/LeftForeArm/LeftHand");
+        Debug.Log("left Hand = " + leftHand.name);
+        rightHand = GameObject.Find(this.gameObject.name + "/CMU compliant skeleton/Hips/LowerBack/Spine/Spine1/RightShoulder/RightArm/RightForeArm/RightHand");
+        leftLeg = GameObject.Find(this.gameObject.name + "/CMU compliant skeleton/Hips/LHipJoint/LeftUpLeg/LeftLeg/LeftFoot");
+        rightLeg = GameObject.Find(this.gameObject.name + "/CMU compliant skeleton/Hips/RHipJoint/RightUpLeg/RightLeg/RightFoot");
+        pelvis = GameObject.Find(this.gameObject.name + "/CMU compliant skeleton/Hips");
+        head = GameObject.Find(this.gameObject.name + "/CMU compliant skeleton/Hips/LowerBack/Spine/Spine1/Neck/Neck1");
+
+        Debug.Log("left Hand Default Local Rotation = " + leftHand.transform.localEulerAngles);
+        leftHand_Default_Rotation = leftHand.transform.localRotation;
+        rightHand_Default_Rotation = rightHand.transform.localRotation;
+        left_Foot_Default_Rotation = leftLeg.transform.localRotation;
+        right_Foot_Default_Rotation = rightLeg.transform.localRotation;
+        pelvis_Default_Rotation = pelvis.transform.localRotation;
+        Debug.Log("Pelvis rotation = " + pelvis_Default_Rotation.eulerAngles);
+        head_Default_Rotation = head.transform.localRotation;
+
+        SteamVR_Activator = GameObject.Find("Steam_VR_Activator_&_Avatar_Handler");
+        m1 = SteamVR_Activator.GetComponent<model_and_Steam_VR_Controller>();
+
+        configuration = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (head_Target == null)
+            head_Target = GameObject.Find("[CameraRig]/Camera (eye)/Head Target");
+        
+        if (pelvis_Target == null)
+            pelvis_Target = GameObject.Find("Other Targets/Pelvis_Bone_Tracker/Pelvis Target");
+
+        if (left_Hand_Target == null)
+            left_Hand_Target = GameObject.Find("Other Targets/Left_Hand_Tracker/Left Hand Target");
+
+        if (right_Hand_Target == null)
+            right_Hand_Target = GameObject.Find("Other Targets/Right_Hand_Tracker/Right Hand Target");
+
+        if (left_Foot_Target == null)
+            left_Foot_Target = GameObject.Find("Other Targets/Left_Foot_Tracker/Left Foot Target");
+
+        if (right_Foot_Target == null)
+            right_Foot_Target = GameObject.Find("Other Targets/Right_Foot_Tracker/Right Foot Target");
+
+        if (head_Target != null && pelvis_Target != null && left_Hand_Target != null && right_Hand_Target != null && left_Foot_Target != null && right_Foot_Target != null && configuration == false)
+        {
+            leftHand_Updated_Rotation = leftHand.transform.localRotation;
+            Debug.Log("left Hand Updated Local Rotation = " + leftHand.transform.localEulerAngles);
+            Debug.Log("leftHandTracker Local Updated = " + left_Hand_Target.transform.localEulerAngles);
+            Quaternion a = leftHand_Default_Rotation * Quaternion.Inverse(leftHand_Updated_Rotation);
+            Quaternion b = left_Hand_Target.transform.localRotation;
+            b = b * a;
+            Debug.Log("Updated Quaternion in Euler Angles = " + b.eulerAngles);
+            left_Hand_Target.transform.localRotation = b;
+
+            rightHand_Updated_Rotation = rightHand.transform.localRotation;
+            Quaternion c = rightHand_Default_Rotation * Quaternion.Inverse(rightHand_Updated_Rotation);
+            Quaternion d = right_Hand_Target.transform.localRotation;
+            d = d * c;
+            right_Hand_Target.transform.localRotation = d;
+
+            left_Foot_Updated_Rotation = leftLeg.transform.localRotation;
+            Quaternion e = left_Foot_Default_Rotation * Quaternion.Inverse(left_Foot_Updated_Rotation);
+            Quaternion f = left_Foot_Target.transform.localRotation;
+            f = f * e;
+            f *= Quaternion.Euler(0, 0, -90);
+            left_Foot_Target.transform.localRotation = f;
+
+            right_Foot_Updated_Rotation = rightLeg.transform.localRotation;
+            Quaternion g = right_Foot_Default_Rotation * Quaternion.Inverse(right_Foot_Updated_Rotation);
+            Quaternion h = right_Foot_Target.transform.localRotation;
+            h = h * g;
+            h *= Quaternion.Euler(0, 0, 90);
+            right_Foot_Target.transform.localRotation = h;
+
+            pelvis_Updated_Rotation = pelvis.transform.localRotation;
+            Quaternion i = pelvis_Default_Rotation * Quaternion.Inverse(pelvis_Updated_Rotation);
+            Quaternion j = pelvis_Target.transform.localRotation;
+            j = j * i;
+            pelvis_Target.transform.localRotation = j;
+
+            head_Updated_Rotation = head.transform.localRotation;
+            Quaternion k = head_Default_Rotation * Quaternion.Inverse(head_Updated_Rotation);
+            Quaternion l = head_Target.transform.localRotation;
+            l = l * k;
+            head_Target.transform.localRotation = l;
+
+            head_Target.transform.localPosition = new Vector3(0, 0.01f, -0.16f);
+            pelvis_Target.transform.localPosition = new Vector3(0, -0.14f, -0.14f);
+            right_Foot_Target.transform.localPosition = new Vector3(0, 0, -0.05f);
+            left_Foot_Target.transform.localPosition = new Vector3(0, 0, -0.05f);
+
+            right_Foot_Target.transform.localRotation = Quaternion.Euler(new Vector3(18, -170, -25));
+            left_Foot_Target.transform.localRotation = Quaternion.Euler(new Vector3(-10, 165, -45));
+            configuration = true;
+        }
+    }
+}
--- Assets/scripts/model_and_Steam_VR_Controller.cs
+++ Assets/scripts/model_and_Steam_VR_Controller.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RootMotion.FinalIK;
+using RootMotion.Demos;
 
 public class model_and_Steam_VR_Controller : MonoBehaviour {
     private GameObject pedestrian, cameraRig, actual_Targets, other_Targets;
     public Transform actualTargets, otherTargets;
     public Transform CameraRig;
-    private bool configuration;
-    private VRIK ik;
-    private GameObject Head, Pelvis, Left_Hand, Right_Hand, Left_Foot, Right_Foot;
+    public bool configuration;
+    private VRIKCalibrationController ik;
+    private VRIK ik1;
+    public GameObject Head, Pelvis, Left_Hand, Right_Hand, Left_Foot, Right_Foot, Head_ik, Pelvis_ik, Left_Hand_ik, Right_Hand_ik, Left_Foot_ik, Right_Foot_ik, left_hand_target_vrik_calibrator, right_hand_target_vrik_calibrator, left_foot_target_vrik_calibrator, right_foot_target_vrik_calibrator, head_target_vrik_calibrator, pelvis_target_vrik_calibrator;
 
     // Use this for initialization
     void Start () {
@@ -19,7 +21,9 @@ public class model_and_Steam_VR_Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (pedestrian == null)
+        {
             pedestrian = GameObject.Find("Sim3");
+        }
 
         if (pedestrian != null && configuration == false)
         {
@@ -29,33 +33,126 @@ public class model_and_Steam_VR_Controller : MonoBehaviour {
             other_Targets = Instantiate(otherTargets, new Vector3(pedestrian.transform.position.x, 0f, pedestrian.transform.position.z), Quaternion.identity).gameObject;
             actual_Targets.name = "Actual Targets";
             other_Targets.name = "Other Targets";
+            pedestrian.AddComponent<Tracker_Automatic_Calibrator>();
             pedestrian.AddComponent<TrackerCalibrationController>();
-            pedestrian.AddComponent<Calibration_Script>();
-            
+            //pedestrian.AddComponent<Calibration_Script>();
+
+            ik1 = pedestrian.GetComponent<VRIK>();
+            //Head_ik = GameObject.Find("[CameraRig]/Camera (eye)/Head");
+            //Pelvis_ik = GameObject.Find("Other Targets/Pelvis_Bone_Tracker/Pelvis");
+            //Left_Hand_ik = GameObject.Find("Other Targets/Left_Hand_Tracker/Left Hand");
+            //Right_Hand_ik = GameObject.Find("Other Targets/Right_Hand_Tracker/Right Hand");
+            //Left_Foot_ik = GameObject.Find("Other Targets/Left_Foot_Tracker/Left Foot");
+            //Right_Foot_ik = GameObject.Find("Other Targets/Right_Foot_Tracker/Right Foot");
+
+            ik = this.gameObject.GetComponent<VRIKCalibrationController>();
+            Head = GameObject.Find("[CameraRig]/Camera (eye)");
+            Pelvis = GameObject.Find("Other Targets/Pelvis_Bone_Tracker");
+            Left_Hand = GameObject.Find("Other Targets/Left_Hand_Tracker");
+            Right_Hand = GameObject.Find("Other Targets/Right_Hand_Tracker");
+            Left_Foot = GameObject.Find("Other Targets/Left_Foot_Tracker");
+            Right_Foot = GameObject.Find("Other Targets/Right_Foot_Tracker");
+
+            //ik1.solver.spine.headTarget = Head_ik.transform;
+            //ik1.solver.spine.pelvisTarget = Pelvis_ik.transform;
+            //ik1.solver.spine.pelvisPositionWeight = 1f;
+            //ik1.solver.spine.pelvisRotationWeight = 1f;
+
+            //ik1.solver.leftArm.target = Left_Hand_ik.transform;
+            //ik1.solver.rightArm.target = Right_Hand_ik.transform;
+
+            //ik1.solver.leftLeg.target = Left_Foot_ik.transform;
+            //ik1.solver.rightLeg.target = Right_Foot_ik.transform;
+            //ik1.solver.leftLeg.positionWeight = 1f;
+            //ik1.solver.leftLeg.rotationWeight = 1f;
+            //ik1.solver.rightLeg.positionWeight = 1f;
+            //ik1.solver.rightLeg.rotationWeight = 1f;
+
+            ik.ik = ik1;
+            ik.headTracker = Head.transform;
+            ik.bodyTracker = Pelvis.transform;
+            ik.leftHandTracker = Left_Hand.transform;
+            ik.leftFootTracker = Left_Foot.transform;
+            ik.rightHandTracker = Right_Hand.transform;
+            ik.rightFootTracker = Right_Foot.transform;
+
+            //ik.data.head.used = true;
+            //ik.data.leftHand.used = true;
+            //ik.data.rightHand.used = true;
+            //ik.data.leftFoot.used = true;
+            //ik.data.rightFoot.used = true;
+            //ik.data.leftLegGoal.used = true;
+            //ik.data.rightLegGoal.used = true;
+            //ik.data.pelvis.used = true;
+
             configuration = true;
 
-            ik = pedestrian.GetComponent<RootMotion.FinalIK.VRIK>();
-            Head = GameObject.Find("[CameraRig]/Camera (eye)/Head");
-            Pelvis = GameObject.Find("Other Targets/Pelvis_Bone_Tracker/Pelvis");
-            Left_Hand = GameObject.Find("Other Targets/Left_Hand_Tracker/Left Hand");
-            Right_Hand = GameObject.Find("Other Targets/Right_Hand_Tracker/Right Hand");
-            Left_Foot = GameObject.Find("Other Targets/Left_Foot_Tracker/Left Foot");
-            Right_Foot = GameObject.Find("Other Targets/Right_Foot_Tracker/Right Foot");
-
-            ik.solver.spine.headTarget = Head.transform;
-            ik.solver.spine.pelvisTarget = Pelvis.transform;
-            ik.solver.spine.pelvisPositionWeight = 1f;
-            ik.solver.spine.pelvisRotationWeight = 1f;
-
-            ik.solver.leftArm.target = Left_Hand.transform;
-            ik.solver.rightArm.target = Right_Hand.transform;
-
-            ik.solver.leftLeg.target = Left_Foot.transform;
-            ik.solver.rightLeg.target = Right_Foot.transform;
-            ik.solver.leftLeg.positionWeight = 1f;
-            ik.solver.leftLeg.rotationWeight = 1f;
-            ik.solver.rightLeg.positionWeight = 1f;
-            ik.solver.rightLeg.rotationWeight = 1f;
+            ////ik.solver.spine.maintainPelvisPosition = 0f;
         }
+
+        if (ik.calibration_done == true)
+        {
+            cameraRig.transform.eulerAngles = new Vector3(cameraRig.transform.eulerAngles.x,
+                                                            90f,
+                                                            cameraRig.transform.eulerAngles.z);
+
+            other_Targets.transform.eulerAngles = new Vector3(other_Targets.transform.eulerAngles.x,
+                                                            90f,
+                                                            other_Targets.transform.eulerAngles.z);
+
+            var new_scale = ik.data.scale;
+            if (new_scale > 1f)
+            {
+                var difference = new_scale - 1f;
+                var new_difference = difference * 3.3f;
+                var updated_scale = 3.3f + new_difference;
+                pedestrian.transform.localScale = new Vector3(updated_scale, updated_scale, updated_scale);
+            }
+
+            else if (new_scale < 1f && new_scale > 0f)
+            {
+                var difference = 1f - new_scale;
+                var new_difference = difference * 3.3f;
+                var updated_scale = 3.3f + new_difference;
+                pedestrian.transform.localScale = new Vector3(updated_scale, updated_scale, updated_scale);
+            }
+
+            else
+                pedestrian.transform.localScale = new Vector3(3.3f, 3.3f, 3.3f);
+
+            cameraRig.transform.localScale = new Vector3(3.3f, 3.3f, 3.3f);
+            other_Targets.transform.localScale = new Vector3(3.3f, 3.3f, 3.3f);
+            ik1.solver.plantFeet = true;
+
+            //rightHand_Updated_Rotation = rightHand.transform.rotation;
+            //left_Foot_Updated_Rotation = leftLeg.transform.rotation;
+            //right_Foot_Updated_Rotation = rightLeg.transform.rotation;
+            //pelvis_Updated_Rotation = pelvis.transform.rotation;
+            //head_Updated_Rotation = head.transform.rotation;
+
+            //left_hand_target_vrik_calibrator = GameObject.Find(Left_Hand.name + "/Left Hand Target");
+            //right_hand_target_vrik_calibrator = GameObject.Find(Right_Hand.name + "/Right Hand Target");
+            //left_foot_target_vrik_calibrator = GameObject.Find(Left_Foot.name + "/Left Foot Target");
+            //right_foot_target_vrik_calibrator = GameObject.Find(Right_Foot.name + "/Right Foot Target");
+            //head_target_vrik_calibrator = GameObject.Find(Head.name + "/Head Target");
+            //pelvis_target_vrik_calibrator = GameObject.Find(Pelvis.name + "/Pelvis Target");
+
+            //head_target_vrik_calibrator.transform.localPosition = new Vector3(0.0f, 0.01f, -0.16f);
+            //pelvis_target_vrik_calibrator.transform.localPosition = new Vector3(0.0f, -0.15f, -0.25f);
+            //left_foot_target_vrik_calibrator.transform.localEulerAngles = new Vector3(-10.0f, 165.0f, -40.0f);
+            //left_hand_target_vrik_calibrator.transform.localEulerAngles = new Vector3(-150.0f, 90.0f, 180.0f);
+            //right_foot_target_vrik_calibrator.transform.localEulerAngles = new Vector3(175.0f, 20.0f, 160.0f);
+            //right_hand_target_vrik_calibrator.transform.localEulerAngles = new Vector3(-60.0f, -90.0f, -180.0f);
+
+            //ik1.solver.leftLeg.bendGoalWeight = 0.0f;
+            //ik1.solver.rightLeg.bendGoalWeight = 0.0f;
+
+            ik.calibration_done = false;
+        }
+
+        if (ik1.solver.leftLeg.bendGoalWeight == 1.0f)
+            ik1.solver.leftLeg.bendGoalWeight = 0.0f;
+        if (ik1.solver.rightLeg.bendGoalWeight == 1.0f)
+            ik1.solver.rightLeg.bendGoalWeight = 0.0f;
     }
 }
