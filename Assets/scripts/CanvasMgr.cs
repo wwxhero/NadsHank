f7b8a0ef8c69fb1a5ae1a4a7a8acab4659d78e86
wanxwang <wanxin-wang@uiowa.edu>
2019-08-20 09:45:16 -0500

    scroll view for showing instruction is added

--- Assets/scripts/CanvasMgr.cs
+++ Assets/scripts/CanvasMgr.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CanvasMgr : MonoBehaviour {
-	public RawImage m_refImg;
+	RawImage m_refImg;		
+	ScrollRect m_refScroll;
 	public bool m_logging = true;
 	// Use this for initialization
 	void Start () {
+		m_refImg = transform.Find("ProtraitView").GetComponent<RawImage>();
+		m_refScroll = transform.Find("ScrollView").GetComponent<ScrollRect>();
         RectTransform this_rct = GetComponent<RectTransform>();
-        RectTransform view_rct = m_refImg.GetComponent<RectTransform>();
-        float this_w = Mathf.Min(this_rct.rect.width, this_rct.rect.height);
-        view_rct.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, this_w);
-        view_rct.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, this_w);
-        view_rct.anchoredPosition = new Vector2(this_rct.rect.width * 0.5f, -this_rct.rect.height * 0.5f);
+        RectTransform spec_rct = m_refImg.GetComponent<RectTransform>();
+        float spec_w = Mathf.Min(this_rct.rect.width, this_rct.rect.height);
+        spec_rct.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, spec_w);
+        spec_rct.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, spec_w);
+        spec_rct.anchoredPosition = new Vector2(this_rct.rect.width * 0.5f, -this_rct.rect.height * 0.5f);
+
+        RectTransform scroll_rct = m_refScroll.GetComponent<RectTransform>();
+        scroll_rct.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, this_rct.rect.height);
+        scroll_rct.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, this_rct.rect.width - spec_w);
+        scroll_rct.anchoredPosition = new Vector2(0, 0);
     }
 
 	// Update is called once per frame
