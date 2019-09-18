4280019d5d7b1af468a1e65bf30f8aca548b5d98
wanxwang <wanxin-wang@uiowa.edu>
2019-09-18 11:44:14 -0500

    ready for test for driver

--- Assets/scripts/SteamVR_ManagerDrv.cs
+++ Assets/scripts/SteamVR_ManagerDrv.cs
@@ -67,7 +67,8 @@ public class SteamVR_ManagerDrv : SteamVR_Manager
 		tracker_start = (int)ObjType.tracker_rhand;
 		tracker_end = (int)ObjType.tracker_lfoot + 1;
 		m_transition = new Transition[] {
-									  new Transition(State.initial, State.pre_cnn, ALL)																												//1
+									  new Transition(State.initial, State.pre_cnn, R_TRIGGER, actGroundEle_r)																						//1
+									, new Transition(State.initial, State.pre_cnn, L_TRIGGER, actGroundEle_l)																						//1.1
 									, new Transition(State.pre_cnn, State.pre_calibra, R_GRIP, new Action[] {actIdentifyTrackers, actConnectVirtualWorld, actShowMirror, actInspecAvatar_f})		//2
 									, new Transition(State.pre_cnn, State.pre_calibra, L_GRIP, new Action[] {actIdentifyTrackers, actConnectVirtualWorld, actShowMirror, actInspecAvatar_f})		//3
 									, new Transition(State.pre_calibra, State.pre_calibra, ALL, actAdjustMirror)																					//4
