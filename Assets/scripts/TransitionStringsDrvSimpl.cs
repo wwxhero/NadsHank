e6d44bd4de21cf0e54ff80d0e0ec455ce2a0dd63
wanxwang <wanxin-wang@uiowa.edu>
2019-09-23 14:39:55 -0500

    complete rewording for driver

--- Assets/scripts/TransitionStringsDrvSimpl.cs
+++ Assets/scripts/TransitionStringsDrvSimpl.cs
@@ -1,39 +1,39 @@
 public class TransitionStringsDrvSimpl
 {
 	public static string [] s_Desc = {
-		  "Press 'Enter' to messure ground elevation."																			//1
-		, "Press 'Enter' to set up the scene."																					//2
-		, "Press 'Down arrow' to move mirror closer, 'Up arrow' to move mirror farthur."										//3
+		  "Press 'Enter' to messure ground elevation."																						//1
+		, "Press 'Enter' to begin calibration."																								//2
+		, "Press 'Down arrow' to move mirror closer, 'Up arrow' to move mirror farthur away."												//3
 		, "Press 'F' to inspect avatar from front."																							//4
 		, "Press 'R' to inspect avatar from right."																							//5
 		, "Press 'U' to inspect avatar from top."																							//6
-		, "Press 'Enter' to lock trackers, and then replace HMD with head tracker."												//7
-		, "Press 'Backspace' to re-align trackers with avatar."																		//10
+		, "Press 'Enter' to attach trackers."																								//7
+		, "Press 'Backspace' to re-align trackers with avatar."																				//10
 		, "Press 'F' to inspect avatar from front."																							//11
 		, "Press 'R' to inspect avatar from right."																							//12
 		, "Press 'U' to inspect avatar from top."																							//13
-		, "Press 'Enter' to complete calibration after replacing HMD with head tracker."											//14
-		, "Press 'Up arrow' to pull avatar upward, 'Down arrow' to push avatar downward"																//16
-		, "Press 'F' to inspect avatar from front."																							//17
-		, "Press 'R' to inspect avatar from right."																							//18
-		, "Press 'U' to inspect avatar from top."																							//19
-		, "Press 'Backspace' to redo encarnation"																						//19.1
-		, "Press 'Enter' to put avatar into car."																				//20
-		, "Put both hands onto steering wheel to be ready for drive, then Press 'Enter'"											//21
-		, "Press 'Enter' while both hands on steering wheel for adjusting position"														//22
-		, "Press 'Enter' while both hands on steering wheel for adjusting position"														//23
-		, "Press 'Enter' while both hands on steering wheel for adjusting position"														//24
+		, "Press 'Enter' to complete tracker setup."																						//14
+		, "Press 'Up arrow' to pull avatar upward, 'Down arrow' to push avatar downward"													//15
+		, "Press 'F' to inspect avatar from front."																							//16
+		, "Press 'R' to inspect avatar from right."																							//17
+		, "Press 'U' to inspect avatar from top."																							//18
+		, "Press 'Backspace' to re-attach trackers."																						//19
+		, "Press 'Enter' to place avatar in car."																							//20
+		, "Press 'Enter' to proceed."																										//21
+		, "Press 'Enter' to re-attach avatar to car."																						//22
+		, "Press 'Enter' to re-attach avatar to car."																						//23
+		, "Press 'Enter' to re-attach avatar to car."																						//24
 		, "Press 'F' to inspect avatar from front.\r\nPress 'R' to inspect avatar from right.\r\nPress 'U' to inspect avatar from top."		//25
 		, null																																//26
 		, "Press 'F' to inspect avatar from front.\r\nPress 'R' to inspect avatar from right.\r\nPress 'U' to inspect avatar from top."		//27
 		, null																																//28
 		, "Press 'F' to inspect avatar from front.\r\nPress 'R' to inspect avatar from right.\r\nPress 'U' to inspect avatar from top."		//29
 		, null																																//30
-		, "Press 'Arrow' to move the avatar relative to the car."																			//31
-		, "Press 'Arrow' to move the avatar relative to the car."																			//32
-		, "Press 'Arrow' to move the avatar relative to the car."																			//33
-		, "Press '(left)SHIFT + M + left Arrow to start over for next participant."														//34
-		, "Press '(left)SHIFT + M + left Arrow to start over for next participant."														//35
-		, "Press '(left)SHIFT + M + left Arrow to start over for next participant."														//36
+		, "Press 'Arrow' keys to adjust avatar position while placing hands on steering wheel and lookforward."								//31
+		, "Press 'Arrow' keys to adjust avatar position while placing hands on steering wheel and lookforward."								//32
+		, "Press 'Arrow' keys to adjust avatar position while placing hands on steering wheel and lookforward."								//33
+		, "Press '(left)SHIFT + M + left Arrow to start over for next participant."															//34
+		, "Press '(left)SHIFT + M + left Arrow to start over for next participant."															//35
+		, "Press '(left)SHIFT + M + left Arrow to start over for next participant."															//36
 	};
 }
